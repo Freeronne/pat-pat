26006a1ef17cf10ae4d54c5fa3eb946b4d66962f
Eronne <erwann.letue@gmail.com>
2021-12-27 13:04:29 +0100

    feat(footer): add home link on logo

--- components/layouts/default/footer-component.vue
+++ components/layouts/default/footer-component.vue
@@ -2,7 +2,9 @@
   <footer>
     <div class="container d-flex justify--space-between align--center full-height">
       <div class="text--center">
-        <img src="@/assets/layout/default/logo.png" alt="" class="logo mb-5">
+        <router-link :to="homeLink">
+          <img src="@/assets/layout/default/logo.png" alt="" class="logo mb-5">
+        </router-link>
         <p>{{ $t('layouts.default.footer.logo-text') }}</p>
       </div>
 
@@ -18,9 +20,14 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
+import { Routes } from '~/constants/routes'
 
 @Component
-export default class HeaderComponent extends Vue {}
+export default class HeaderComponent extends Vue {
+  get homeLink() {
+    return this.localePath(Routes.Home.name, this.$i18n.locale);
+  }
+}
 </script>
 
 <style scoped lang="scss">
