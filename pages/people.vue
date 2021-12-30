c9d5ecdb91c26d40fa2d5d7426eba958acc3a1c1
Eronne <erwann.letue@gmail.com>
2021-12-30 12:13:16 +0100

    fix(people): correct feedbacks

--- pages/people.vue
+++ pages/people.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <h1 class="title">{{ $t('pages.people.title') }}</h1>
-
     <div class="mb-16">
-      <h2>{{ $t('pages.people.bureau.title') }}</h2>
+      <h1>{{ $t('pages.people.association.title') }}</h1>
 
       <div class="people-container d-flex flex-wrap justify--space-between align--center">
         <bureau-member-card v-for="bureauMember in bureauMembers" :key="bureauMember.name" :member="bureauMember" />
@@ -12,13 +10,17 @@
       </div>
     </div>
 
-    <div>
-      <h2 class="partners-title text--center">{{ $t('pages.people.partners.title') }}</h2>
+    <div class="mb-16">
+      <h1>{{ $t('pages.people.partners.title') }}</h1>
 
       <div class="people-container d-flex flex-wrap justify--space-between align--center">
         <img v-for="partnersImage in partnersImages" :key="partnersImage" :src="partnersImage" alt="">
       </div>
-      <p class="mt-10 text--center">{{ $t('pages.people.partners.text') }}</p>
+    </div>
+
+    <div>
+      <h1>{{ $t('pages.people.ambassadors.title') }}</h1>
+      <p class="text--center fs--medium">{{ $t('pages.people.ambassadors.text') }}</p>
     </div>
   </div>
 </template>
@@ -39,14 +41,14 @@ export default class PeoplePage extends Vue {
       {
         color: '#70C8E5',
         image: BureauMembersImages.Pascale,
-        name: this.$t('pages.people.bureau.members.pascale.name') as string,
-        jobDescription: this.$t('pages.people.bureau.members.pascale.job-description') as string,
+        name: this.$t('pages.people.association.members.pascale.name') as string,
+        jobDescription: this.$t('pages.people.association.members.pascale.job-description') as string,
       },
       {
         color: '#E2C44B',
         image: BureauMembersImages.Arnaud,
-        name: this.$t('pages.people.bureau.members.arnaud.name') as string,
-        jobDescription: this.$t('pages.people.bureau.members.arnaud.job-description') as string,
+        name: this.$t('pages.people.association.members.arnaud.name') as string,
+        jobDescription: this.$t('pages.people.association.members.arnaud.job-description') as string,
       },
     ]
   }
@@ -58,22 +60,18 @@ export default class PeoplePage extends Vue {
 </script>
 
 <style scoped lang="scss">
-.title {
-  color: #80BA42;
+h1 {
+  color: #70C8E5;
 }
 
 .people-container {
   & > * {
-    width: 30%;
+    width: 27%;
 
     @include media-max('md') {
       margin: 32px auto 32px; // TODO: Use extends
       width: 75%;
     }
   }
-}
-
-.partners-title {
-  color: #70C8E5;
 }
 </style>
